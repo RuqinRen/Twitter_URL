49c249c9144f280999d2137282112b45ff48f0ad
RuqinRen <ruqinren@usc.edu>
2021-01-22 16:01:45 +0800

    Update cleanup.R

--- cleanup.R
+++ cleanup.R
@@ -1,22 +1,33 @@
 ds <- read.csv("total7&8.csv", header = T)
+ds2 <- read.csv("totalOct.csv", header = T)
+ds3<- read.csv("total11&12.csv", header = T)
+
 names(ds)
-length(levels(ds[,26])) #around 13029 unique url/ some share two ore more urls
-13029/174893 # 0.074
-summary(ds[,18])
+colnames(ds2) <- colnames(ds)
+colnames(ds3) <- colnames(ds)
+
+ds_large <- rbind(ds, ds2, ds3)
+remove(ds, ds2, ds)
+
+length(levels(ds_large[,26])) #around 13029 unique url/ some share two ore more urls
+
+summary(ds_large[,18]) #retweet count
 
 #remove original content, only keep RT
 library(dplyr)
-ds_allurl <- ds %>% 
-  filter(str_detect(Text, "RT ")) %>% 
-  filter( !(X.M..url_mention_expanded.=="")  ) %>%
-  select(3,18,24,26)
 
-ds_onlytwitter <- ds_allurl %>%
-  filter(  str_detect(X.M..url_mention_expanded., "https://twitter.com") )  %>%
-  rename( mention_twitter_url =X.M..url_mention_expanded.)
+ds_allurl <- ds_large %>% 
+  filter(str_detect(Text, "RT ")) %>%
+  select(3,24,26) %>%
+  filter( !(X.M..url_mention_expanded.=="")  ) 
+  
+ds_onlyexternal <- ds_allurl %>%
+  filter( !str_detect(X.M..url_mention_expanded., "https://twitter.com") )  %>%
+  rename( mention_ext_url =X.M..url_mention_expanded.) %>%
+  distinct(mention_ext_url)
 
-ds_onlyexternal<- anti_join(ds_allurl, ds_onlytwitter, by = c("X.M..url_mention_expanded." = "mention_twitter_url") )
-colnames(ds_onlyexternal)[4] <- "mention_ext_url"
+#ds_onlyexternal<- anti_join(ds_allurl, ds_onlytwitter, by = c("X.M..url_mention_expanded." = "mention_twitter_url") )
+#colnames(ds_onlyexternal)[4] <- "mention_ext_url"
 
 ds_onlytwitter$mention_ext_url <- NA
 ds_onlyexternal$mention_twitter_url <- NA
@@ -30,7 +41,5 @@ write.csv(both_url, "both_url.csv")
 table(unlist(Map(grepl, "https://twitter.com", urlds)))
 #FALSE  TRUE 
 #1224   11805
-
-1224/13029 
-#0.09 are from outside of Twitter links. Nearly 90% are linking to another tweet url.
-
+1224/13029 #0.09 are from outside of Twitter links. Nearly 90% are linking to another tweet url.
+head(ds[16])
